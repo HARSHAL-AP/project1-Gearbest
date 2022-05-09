d4cdb8e18419858c2297a18419fd5fe3f358ae71
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-05-09 12:50:53 +0530

    timer upadated on home page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("Jan 6, 2024 12:00:00").getTime();
 
 var x = setInterval(function() {
 
@@ -6,7 +6,7 @@ var x = setInterval(function() {
     var now = new Date().getTime();
       
   
-    var distance = countDownDate - now;
+    var distance = countDownDate -now;
       
    
     
